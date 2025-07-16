4e22f5190f266adf111155f2811c72e7b476e1e4
matti909 <matias.saantiago@gmail.com>
2025-07-16 09:12:39 -0300

    markdown added

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import post_list, post_detail, post_comment, post_share
-
+from .feeds import LatestPostsFeed
 
 app_name = "blog"
 
@@ -13,4 +13,5 @@ urlpatterns = [
     ),
     path("<int:post_id>/share/", post_share, name="post_share"),
     path("<int:post_id>/comment/", post_comment, name="post_comment"),
+    path("feed/", LatestPostsFeed(), name="post_feed"),
 ]
