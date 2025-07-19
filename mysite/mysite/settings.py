cce9553370a3c92df79d85df9d76fc3e043914d3
matti909 <matias.saantiago@gmail.com>
2025-07-19 18:47:03 -0300

    fixed deploy

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -4,8 +4,8 @@ from decouple import config
 
 
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="matias.saantiago@gmail.com")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
